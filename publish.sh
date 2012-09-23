7bfcca3ce0e6e182720886e83198e7986aba9341
Steve Losh <steve@stevelosh.com>
2012-09-23 12:27:43 -0400

    Tweak build process and update outline.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-./venv/bin/python ../bookmarkdown/bookmarkdown/bookmarkdown build
+python ../bookmarkdown/bookmarkdown/bookmarkdown build
 rsync --delete -az build/html/ sl:/var/www/vimscript/
