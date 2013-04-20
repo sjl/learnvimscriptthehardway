a6999f4b281483664e5c12a7e943375ab7571a47
isayme <isaymeorg@gmail.com>
2013-04-19 21:00:23 -0700

    my own publisher

--- publish.sh
+++ publish.sh
@@ -2,4 +2,7 @@
 
 set -e
 ./build.sh
-rsync --delete -az build/html/ sl:/var/www/vimscript/
+#rsync --delete -az build/html/ sl:/var/www/vimscript/
+cd build/html                                                                                                 
+scp -r -P 12306 ./ root@onefloweroneworld:/home/learnvimscriptthehardway
+cd ../..
